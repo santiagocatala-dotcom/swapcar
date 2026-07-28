c04a65c054bf72ec5cb9eb8f9c2e2f9ddf9babd4
Santiago Catala <santiagocatala@gmail.com>
2026-07-28 00:01:58 -0300

    fix: logo fijo en esquina superior izquierda

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -160,9 +160,9 @@ export default function ProfilePage() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col">
       {/* App header */}
-      <div className="px-4 pt-4">
-        <div className="w-32 h-32 mx-auto">
-          <img src="/logo.png" alt="REVE" className="w-full h-full object-contain" />
+      <div className="fixed top-4 left-4 z-50">
+        <div className="w-32 h-32">
+          <img src="/logo.png" alt="REVE" className="w-full h-full object-contain drop-shadow-lg" />
         </div>
       </div>
       <div className="flex-1 max-w-lg mx-auto w-full px-4 pt-2 pb-24">
